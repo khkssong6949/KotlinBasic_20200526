f0f01ba56adfd6b8fefe7d6e15fdc751f139c04c
khkssong6949 <bada6949@gmail.com>
2020-05-26 21:59:40 +0900

    버튼이 눌리면 EditText에 저장된 내용을 추출=>TextView에 반영

--- app/src/main/java/kr/co/hgney/kotlinbasic_20200526/MainActivity.kt
+++ app/src/main/java/kr/co/hgney/kotlinbasic_20200526/MainActivity.kt
@@ -2,11 +2,19 @@ package kr.co.hgney.kotlinbasic_20200526
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        okBtn.setOnClickListener {
+
+            val inputContent = inputEtxt.text.toString()
+            resultTxt.text = inputContent
+        }
+
     }
 }
